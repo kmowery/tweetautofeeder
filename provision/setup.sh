d9e27669c0ee189ee964e049c80123c034623080
kmowery <kmowery@gmail.com>
2013-12-15 23:41:15 -0800

    preconfigure golang-go so it installs cleanly

--- provision/setup.sh
+++ provision/setup.sh
@@ -2,6 +2,9 @@
 
 apt-get update
 apt-get -y install git
+
+# preconfigure and install golang
+echo "golang-go golang-go/dashboard boolean false" | debconf-set-selections
 apt-get -y install golang
 
 go get github.com/gorilla/mux
