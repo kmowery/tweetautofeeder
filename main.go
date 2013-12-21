8aaedfc9fe3e53f237b3eff5b9e591b9cb07e7fd
kmowery <kmowery@gmail.com>
2013-12-20 16:09:27 -0800

    Add /static/ and a redirect from / to /blog

--- main.go
+++ main.go
@@ -6,13 +6,28 @@ import (
   "github.com/gorilla/mux"
 )
 
+func makeIndexHandler(router mux.Router) http.HandlerFunc {
+  return func (w http.ResponseWriter, r *http.Request) {
+    s,err := router.Get("blog").URLPath()
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    http.Redirect(w, r, s.String(), 307)
+    return
+  }
+}
+
 func main() {
     var err error
     err = nil
 
     r := mux.NewRouter()
-    r.Handle("/", http.FileServer(http.Dir("/usr/share/3amh/www")))
-    r.HandleFunc("/blog", blogHandler )
+    r.HandleFunc("/", makeIndexHandler(*r) ).Name("root")
+    r.HandleFunc("/blog", blogHandler ).Name("blog")
+
+    r.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("/usr/share/3amh/www"))))
+
     err = http.ListenAndServe(":8080", r)
     if err != nil {
       log.Fatal(err)
