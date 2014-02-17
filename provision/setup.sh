f9749caa55877b180736c377aea0bda0dd90de38
kmowery <kmowery@gmail.com>
2014-02-16 21:00:18 -0800

    Switch to go 1.1

--- provision/setup.sh
+++ provision/setup.sh
@@ -3,6 +3,10 @@
 apt-get update
 apt-get -y install git
 
+apt-get -y install python-software-properties
+add-apt-repository -y ppa:duh/golang
+apt-get update
+
 # preconfigure and install golang
 echo "golang-go golang-go/dashboard boolean false" | debconf-set-selections
 apt-get -y install golang
@@ -14,5 +18,6 @@ chown -R vagrant:vagrant ${GOPATH}
 
 go get github.com/gorilla/mux
 go get github.com/hoisie/mustache
+go get github.com/ChimeraCoder/anaconda
 
 
